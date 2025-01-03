2df297c94598a2e5d4410050c1bba0a5fad0f843
Matt Taylor <matt@matthewjosephtaylor.com>
2025-01-03 10:58:34 -0600

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,59 @@
 import { Avatar3d } from "@mjtdev/avatar-3d";
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+// import { PhonemeLevelsDisplay, Vads } from "@mjtdev/vad-2025";
+import { Button, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { Stack } from "@mui/system";
+import { useState } from "react";
 
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
   },
 });
-
-export const App = () => (
-  <ThemeProvider theme={darkTheme}>
-    <CssBaseline />
-    <Avatar3d
-      canvasWidth={768}
-      canvasHeight={1920}
-      path={"fem.vrm"}
-      style={{
-        // backgroundColor: "purple",
-        // border: "1px solid white",
-        // maxWidth: "768",
-        // maxHeight: "1024",
-        width: '100%',        // Set width as needed
-        height: '100vh',      // Set height as needed
-        backgroundImage: "url('monet.jpg')",
-        backgroundSize: 'auto',         // Keeps the image at its natural size
-        // backgroundPosition: 'center',   // Centers the image
-        backgroundRepeat: 'no-repeat',  // Prevents tiling
-        overflow: 'hidden',             // Hides any overflow content
-      }}
-      vrmCameraOptions={{
-        frustumSize: 1.5,
-
-      }}
-      animationPath="Idle_2.fbx"
-    />
-  </ThemeProvider>
-);
+export const App = () => {
+  const [active, setActive] = useState(false);
+  // const analyserNode = Vads.useMicAudio(active);
+  // console.log("PhonemeLevelsDisplay", PhonemeLevelsDisplay);
+  // console.log("analyzerNode", analyserNode);
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />{" "}
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          console.log("Start button clicked");
+          setActive((a) => !a);
+          // getMicrophoneInput();
+          // startVad();
+        }}
+      >
+        Microphone {active ? "on" : "off"}
+      </Button>
+      <Stack spacing={2}>
+        {/* {analyserNode && <PhonemeLevelsDisplay analyserNode={analyserNode} />} */}
+        <Avatar3d
+          canvasWidth={768}
+          canvasHeight={1920}
+          path={"fem.vrm"}
+          style={{
+            // backgroundColor: "purple",
+            // border: "1px solid white",
+            // maxWidth: "768",
+            // maxHeight: "1024",
+            width: "100%", // Set width as needed
+            height: "100vh", // Set height as needed
+            backgroundImage: "url('monet.jpg')",
+            backgroundSize: "auto", // Keeps the image at its natural size
+            // backgroundPosition: 'center',   // Centers the image
+            backgroundRepeat: "no-repeat", // Prevents tiling
+            overflow: "hidden", // Hides any overflow content
+          }}
+          vrmCameraOptions={{
+            frustumSize: 1.5,
+          }}
+          animationPath="Idle_2.fbx"
+        />
+      </Stack>
+    </ThemeProvider>
+  );
+};
